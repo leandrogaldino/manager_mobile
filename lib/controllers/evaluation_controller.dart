420deb737a00fe95f944bd22d6751c245d7cd058
Leandro Correia Galdino <leandrocgaldino@gmail.com>
2025-01-27 21:12:18 -0300

    Update evaluation_controller.dart

--- lib/controllers/evaluation_controller.dart
+++ lib/controllers/evaluation_controller.dart
@@ -8,7 +8,33 @@ class EvaluationController extends ChangeNotifier {
   bool _isSignatureSectionExpanded = false;
 
   bool get isReadingSectionExpanded => _isReadingSectionExpanded;
+  bool get isCoalescentSectionExpanded => _isCoalescentSectionExpanded;
+  bool get isTechnicianSectionExpanded => _isTechnicianSectionExpanded;
+  bool get isPhotoSectionExpanded => _isPhotoSectionExpanded;
+  bool get isSignatureSectionExpanded => _isSignatureSectionExpanded;
 
-  //TODO: a
-  //BUG: b
+  void toggleReadingSection() {
+    _isReadingSectionExpanded = !isReadingSectionExpanded;
+    notifyListeners();
+  }
+
+  void toggleCoalescentSection() {
+    _isCoalescentSectionExpanded = !isCoalescentSectionExpanded;
+    notifyListeners();
+  }
+
+  void toggleTechnicianSection() {
+    _isTechnicianSectionExpanded = !isTechnicianSectionExpanded;
+    notifyListeners();
+  }
+
+  void togglePhotoSection() {
+    _isPhotoSectionExpanded = !isPhotoSectionExpanded;
+    notifyListeners();
+  }
+
+  void toggleSignatureSection() {
+    _isSignatureSectionExpanded = !isSignatureSectionExpanded;
+    notifyListeners();
+  }
 }
