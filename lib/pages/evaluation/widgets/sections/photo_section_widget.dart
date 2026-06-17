ce17e14e358484755c8f29aa1dd3f41e9782084c
Leandro Correia Galdino <leandrocgaldino@gmail.com>
2026-06-17 20:00:47 -0300

    Update photo_section_widget.dart

--- lib/pages/evaluation/widgets/sections/photo_section_widget.dart
+++ lib/pages/evaluation/widgets/sections/photo_section_widget.dart
@@ -7,6 +7,8 @@ import 'package:manager_mobile/core/util/internet_connection.dart';
 import 'package:manager_mobile/core/util/message.dart';
 import 'package:manager_mobile/models/evaluation_photo_model.dart';
 import 'package:manager_mobile/core/enums/source_types.dart';
+import 'package:manager_mobile/pages/photos/photos_page.dart';
+import 'package:timezone/timezone.dart';
 
 class PhotoSectionWidget extends StatefulWidget {
   const PhotoSectionWidget({
@@ -48,23 +50,14 @@ class _PhotoSectionWidgetState extends State<PhotoSectionWidget> {
           final localPath = photo?.localPath;
           final cloudPath = photo?.cloudPath;
           final bool isPhotoTaken = localPath != null || cloudPath != null;
+          final state = _getPhotoState(tempPath, localPath, cloudPath);
           return GestureDetector(
             onTap: () async {
-              if (cloudPath != null && localPath == null && tempPath == null) {
-                if (_downloadingPhotos.contains(cloudPath)) {
-                  return;
-                }
-
+              if (state == PhotoState.downloading) return;
                 final hasInternet = await InternetConnectionStream.hasInternetNow();
-
                 if (!hasInternet) {
                   if (!context.mounted) return;
-
-                  Message.showInfoSnackbar(
-                    context: context,
-                    message: 'Sem conexão com a internet',
-                  );
-
+                  Message.showInfoSnackbar(context: context,message: 'Sem conexão com a internet');
                   return;
                 }
 
@@ -126,60 +119,99 @@ class _PhotoSectionWidgetState extends State<PhotoSectionWidget> {
                 border: Border.all(color: Theme.of(context).colorScheme.primary, width: 2),
               ),
               child: _downloadingPhotos.contains(cloudPath)
-                  ? const Center(
-                      child: SizedBox(
-                        width: 30,
-                        height: 30,
-                        child: CircularProgressIndicator(),
-                      ),
-                    )
+                  ? _downloadingWidget()
                   : isPhotoTaken
                       ? localPath != null
-                          ? ClipRRect(
-                              borderRadius: BorderRadius.circular(8),
-                              child: Image.file(
-                                File(localPath),
-                                fit: BoxFit.cover,
-                                width: double.infinity,
-                                height: double.infinity,
-                                filterQuality: FilterQuality.low,
-                                cacheWidth: 600,
-                                gaplessPlayback: true,
-                                errorBuilder: (_, __, ___) {
-                                  return const Center(
-                                    child: Icon(Icons.broken_image),
-                                  );
-                                },
-                              ),
-                            )
-                          : Icon(
-                              Icons.cloud_download,
-                              color: Theme.of(context).colorScheme.primary,
-                              size: 38,
-                            )
-                      : Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.camera_alt,
-                                size: 40,
-                                color: Theme.of(context).colorScheme.primary,
-                              ),
-                              const SizedBox(height: 8),
-                              Text(
-                                'Adicionar Foto',
-                                style: Theme.of(context).textTheme.labelMedium!.copyWith(
-                                      color: Theme.of(context).colorScheme.primary,
-                                    ),
-                              ),
-                            ],
-                          ),
-                        ),
+                          ? _viewLocalPhotoWidget(localPath)
+                          : _downloadPhotoWidget(context)
+                      : _emptyPhotoWidget(),
             ),
           );
         },
       ),
     );
   }
+
+  PhotoState _getPhotoState(String? tempPath, String? localPath, String? cloudPath) {
+    if (cloudPath != null && _downloadingPhotos.contains(cloudPath)) return PhotoState.downloading;
+    if (tempPath != null) return PhotoState.temp;
+    if (localPath != null) return PhotoState.local;
+    if (cloudPath != null) return PhotoState.cloud;
+    return PhotoState.empty;
+  }
+
+  Icon _downloadPhotoWidget(BuildContext context) {
+    return Icon(
+      Icons.cloud_download,
+      color: Theme.of(context).colorScheme.primary,
+      size: 38,
+    );
+  }
+
+  ClipRRect _viewLocalPhotoWidget(String localPath) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: Image.file(
+        File(localPath),
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
+        filterQuality: FilterQuality.low,
+        cacheWidth: 600,
+        gaplessPlayback: true,
+        errorBuilder: (_, __, ___) {
+          return const Center(
+            child: Icon(Icons.broken_image),
+          );
+        },
+      ),
+    );
+  }
+
+  Center _downloadingWidget() {
+    return const Center(
+      child: SizedBox(
+        width: 30,
+        height: 30,
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
+
+class _emptyPhotoWidget extends StatelessWidget {
+  const _emptyPhotoWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.camera_alt,
+            size: 40,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+          const SizedBox(height: 8),
+          Text(
+            'Adicionar Foto',
+            style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+enum PhotoState {
+  empty,
+  downloading,
+  cloud,
+  local,
+  temp,
 }
