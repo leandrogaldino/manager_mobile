a23deb944afe6379521ec5e932e39083f881cde7
Leandro Correia Galdino <leandrocgaldino@gmail.com>
2026-06-18 20:54:20 -0300

    Update take_photo_page.dart

--- lib/pages/photos/take_photo_page.dart
+++ lib/pages/photos/take_photo_page.dart
@@ -4,23 +4,21 @@ import 'package:image_picker/image_picker.dart';
 import 'package:manager_mobile/controllers/evaluation_controller.dart';
 import 'package:manager_mobile/core/enums/image_types.dart';
 import 'package:manager_mobile/services/image_service.dart';
+import 'package:manager_mobile/core/locator.dart';
 
 class TakePhotoPage extends StatefulWidget {
-  const TakePhotoPage({
-    super.key,
-    required this.evaluationController,
-  });
-
-  final EvaluationController evaluationController;
+  const TakePhotoPage({super.key});
   @override
   State<TakePhotoPage> createState() => _TakePhotoPageState();
 }
 
 class _TakePhotoPageState extends State<TakePhotoPage> {
+  late final EvaluationController _controller;
+
   @override
   void initState() {
     super.initState();
-
+    _controller = Locator.get<EvaluationController>();
     openCamera();
   }
 
