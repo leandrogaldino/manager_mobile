eb7f77e518d303d4b96fb3cbb278db1e3f2c6c35
Leandro Correia Galdino <62770546+leandrogaldino@users.noreply.github.com>
2026-07-23 09:53:35 -0300

    Update interface_direction_types.dart

--- lib/core/enums/interface_direction_types.dart
+++ lib/core/enums/interface_direction_types.dart
@@ -1,9 +1,12 @@
 enum InterfaceDirectionTypes {
+  none,
   ascending,
   descending;
 
   String get stringValue {
     switch (this) {
+      case InterfaceDirectionTypes.none:
+        return 'N/A';
       case InterfaceDirectionTypes.ascending:
         return 'Crescente';
       case InterfaceDirectionTypes.descending:
